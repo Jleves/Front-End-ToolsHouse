858de97889a0252358e3f48842ec6c1a9ecf3d94
jleves91 <jlinformatica91@gmail.com>
2024-05-16 07:30:38 -0300

    build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"http://172.17.0.2/root/ToolsHouse-Front-End.git"
+ 
 });
